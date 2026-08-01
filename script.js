a63404eb723fbb39fa2bb079e897363aaaf63368
Inderjeet Prasad <ijprasad@gmail.com>
2026-08-01 19:38:19 +0530

    Added Script js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Display a message when the page loads
+console.log("Hello, World!");
+
+// Variables
+const userName = "Inderjeet";
+let count = 0;
+
+// Function
+function greet(name) {
+    return `Welcome, ${name}!`;
+}
